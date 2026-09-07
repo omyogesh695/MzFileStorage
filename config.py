29b3ac8b8a17e52b1f291a37ef051d8e883b551e
omyogesh695 <108509038+omyogesh695@users.noreply.github.com>
2026-09-07 12:15:06 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,7 +7,6 @@ class Config:
 
     # Your Bot Token
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-    SESSION_STRING = os.environ.get("SESSION_STRING", "")
 
     # Your Admin User ID
     ADMIN_ID = int(os.environ.get("ADMIN_ID", ""))
@@ -15,8 +14,6 @@ class Config:
     # Your Owner DB Channel ID
     OWNER_DB_CHANNEL = int(os.environ.get("OWNER_DB_CHANNEL", "-1003433884727"))
     LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1004424440786"))  # Aapka Master Admin Log Channel
-    OWNER_DB_INVITE_LINK = os.environ.get("OWNER_DB_INVITE_LINK", "")
-    LOG_CHANNEL_INVITE_LINK = os.environ.get("LOG_CHANNEL_INVITE_LINK", "")
 
     # Your MongoDB Connection String
     MONGO_URI = os.environ.get("MONGO_URI", "")
